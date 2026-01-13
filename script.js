52b12650ed2a1752bbdfdcad0189abe434c5c7e6
muhamad adli <muhmdadli01@gmail.com>
2026-01-13 23:57:19 +0700

    start project

---
+++ script.js
@@ -0,0 +1,15 @@
+// navbar 
+// const navbar = document.querySelector('.navbar ul');
+// const nav = document.querySelector('.navbar');
+
+// document.querySelector('#menu').onclick = () => {
+//     navbar.classList.toggle('slide');
+// };
+
+// window.addEventListener('scroll' , () =>{
+//     const windowPosition = window.scrollY > 0 ;
+//     nav.classList.toggle('scrolled' , windowPosition);
+//     console.log(window.screenY);
+// });
+
+
